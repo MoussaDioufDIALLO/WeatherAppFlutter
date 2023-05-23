c2a5700bf800d9298eeecfcafc18d640e9cdf2f6
Moussa Diouf <moussadioufdiallo@mail.com>
2023-05-23 20:13:00 +0000

    23/Mai/2023-20h

--- lib/Views/welcome_page.dart
+++ lib/Views/welcome_page.dart
@@ -7,13 +7,10 @@ import 'package:weather/controlers/currentWeather.dart';
 import 'package:weather/controlers/delayed_animation.dart';
 import 'package:weather/main.dart';
 
-
 class WelcomePage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xFFEDECF2),
       body: SingleChildScrollView(
         child: Container(
@@ -22,26 +19,36 @@ class WelcomePage extends StatelessWidget {
             horizontal: 30,
           ),
           child: Column(
-            children: [DelayedAnimation(
+            children: [
+              SizedBox(height: 20), // Espace supplémentaire
+              Text(
+                "MétéoExpress",
+                style: GoogleFonts.poppins(
+                  color: d_red,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DelayedAnimation(
                 delay: 1500,
                 child: Container(
-                  margin:EdgeInsets.only(top:180),
+                  margin: EdgeInsets.only(top: 180),
                   height: 170,
                   child: Image.asset('images/neige.png'),
-                )
-            ),
+                ),
+              ),
               DelayedAnimation(
                 delay: 2500,
                 child: Container(
                   margin: EdgeInsets.only(
-                      top: 30,
-                      bottom: 20
+                    top: 30,
+                    bottom: 20,
                   ),
                   child: Text(
                     "Soyez à jour avec les dernières conditions météorologiques et soyez prêt pour n'importe quelle prévision. Obtenez des informations précises sur la température, le vent et les précipitations pour planifier votre journée en conséquence.",
                     style: GoogleFonts.poppins(
-                        color: Colors.black87,
-                        fontSize: 16
+                      color: Colors.black87,
+                      fontSize: 16,
                     ),
                   ),
                 ),
@@ -50,19 +57,19 @@ class WelcomePage extends StatelessWidget {
                 delay: 2500,
                 child: Container(
                   width: double.infinity,
-                  child:ElevatedButton(
-                    style:ElevatedButton.styleFrom(
-                        backgroundColor: d_red,
-                        shape:  StadiumBorder(),
-                        padding: EdgeInsets.all(13)
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: d_red,
+                      shape: StadiumBorder(),
+                      padding: EdgeInsets.all(13),
                     ),
                     child: Text('COMMENCER'),
-                    onPressed:() {
+                    onPressed: () {
                       /*  Navigator.push(context, MaterialPageRoute(builder: (context) => SocialPage(),
                               Get.to(SocialPage());
                            ),
-                          );*/   Get.to(CurrentWeatherPage());
-
+                          );*/
+                      Get.to(CurrentWeatherPage());
                     },
                   ),
                 ),
